c6061f70d75a96af529d5048361d5d79244fa0c2
arif <maknetwork>
2020-03-16 01:47:53 +0530

    Job Scheduling Working ,fixed home switches

--- home/urls.py
+++ home/urls.py
@@ -15,7 +15,7 @@ path('edit_control', views.editcontrol, name='editcontrol'),
 
     url(r'^savedata/$', views.saveflybox, name='saveflybox'),
         url(r'^update_control/$', views.updatecontrol, name='update_control'),
-                url(r'^startautomation/(?P<categ_name>\w+)/(?P<subcateg_name>\w+)/$', views.startautomation, name='start_automation'),
+                url(r'^startautomation/$', views.startautomation, name='start_automation'),
 
         url(r'^update_controlpd/$', views.updatecontrolpd, name='update_controlpd'),
 
--- home/views.py
+++ home/views.py
@@ -6,10 +6,14 @@ from .models import Flyboxset, Setup, Automation,Automationpd
 from django.views.generic.edit import CreateView  # new
 from django.http import HttpResponse, HttpResponseRedirect
 from bs4 import BeautifulSoup
-import requests 
+import requests
 import pytz
 from django.http import HttpResponse, JsonResponse
 from django_celery_beat.models import CrontabSchedule, PeriodicTask
+from crontab import CronTab
+
+
+cron = CronTab(user='arif')# change the user name
 
 def homepage(request):
     posts = Flyboxset.objects.all()
@@ -49,9 +53,9 @@ def saveflybox(request):
             title = request.POST.get('name')
             ipaddress = request.POST.get('ipaddress')
             URL = "http://%s" %ipaddress
-            r = requests.get(URL) 
-              
-            soup = BeautifulSoup(r.content, 'html5lib') 
+            r = requests.get(URL)
+
+            soup = BeautifulSoup(r.content, 'html5lib')
             inputs=soup.find("input", {"id": "input-id"})
             if inputs['value']== "hello":
                 user = Flyboxset.objects.create(title=title, ipaddress=ipaddress)
@@ -78,7 +82,7 @@ def disconnectflybox(request):
             return JsonResponse({'success':True})
         except:
             return JsonResponse({'error':"false"})
- 
+
 
     else:
 
@@ -95,37 +99,34 @@ def updatecontrol(request):
         obj.save()
         print(timecd)
         my_string = ','.join(map(str, cddays))
-        cdtimelist= timecd.split(':') 
+        cdtimelist= timecd.split(':')
         print(cdtimelist[0])
         cronnamer= Flyboxset.objects.get(pk=flyboxkey)
         cronname= cronnamer.title
         cronnameid= cronnamer.pk
-
+        print((cronnamer.title+"cd"))
         try:
-            obj2 = PeriodicTask.objects.get(name=cronname)
+            obj2 = PeriodicTask.objects.get(name=(cronname+"cd"))
 
         except PeriodicTask.DoesNotExist:
-
+            commander= str( cdtimelist[1])+" "+ str(cdtimelist[0]) +' * * '+str(my_string)
 
             schedule, _ = CrontabSchedule.objects.get_or_create(minute=cdtimelist[1],hour=cdtimelist[0],day_of_week=my_string,day_of_month='*', month_of_year='*')
-            PeriodicTask.objects.create(crontab=schedule, name=cronname, task='flybox.celery.see_you', enabled=False)
-
-            f = open('/home/arif/flybox/flybox/flybox/celery.py','a')
-            f.write('\n'+ '@app.task')
-
-
-            f.write('\n'+ 'def cd'+ str(cronnameid) +'():')
-            f.write('\n'+ '    print("it working")')
-            f.write('\n'+ '    contents = urllib.request.urlopen("http://'+cronnamer.ipaddress +'/5/on").read()')
-             
-
-            f.close()
+            PeriodicTask.objects.create(crontab=schedule, name=(cronname+"cd"), task=('flybox.celery.cd' + str(cronnameid) +'()'), enabled=False)
+            job = cron.new(command= 'curl '+ cronnamer.ipaddress +"/5/on", comment=(cronnamer.title+"cd"))
+            job.minute.every(str(int(cdtimelist[1])))
+            job.hour.every(str(int(cdtimelist[0])))
+            job.dow.on(*cddays)
+            job.enable()
+            cron.write()
 
             return HttpResponse('Successfull')
         else:
             schedule2, _ = CrontabSchedule.objects.get_or_create(minute=cdtimelist[1],hour=cdtimelist[0],day_of_week=my_string,day_of_month='*', month_of_year='*',)
-            obj2.crontab=schedule2 
+            obj2.crontab=schedule2
             obj2.save
+            print(str(int(cdtimelist[0])))
+
             return HttpResponse("Successfully updated")
 
 def updatecontrolpd(request):
@@ -138,7 +139,7 @@ def updatecontrolpd(request):
         obj = Automationpd(flybox=Flyboxset.objects.get(title=flyboxkey), pddays=pddays,pddtime=timepd,setupcompleted=True)
         obj.save()
         my_string = ','.join(map(str,pddays))
-        pdtimelist= timepd.split(':') 
+        pdtimelist= timepd.split(':')
         print(pdtimelist[0])
         cronnamer= Flyboxset.objects.get(title=flyboxkey)
         cronname= cronnamer.title
@@ -149,28 +150,24 @@ def updatecontrolpd(request):
 
         except PeriodicTask.DoesNotExist:
 
-
             schedule, _ = CrontabSchedule.objects.get_or_create(minute=pdtimelist[1],hour=pdtimelist[0],day_of_week=my_string,day_of_month='*', month_of_year='*')
             PeriodicTask.objects.create(crontab=schedule, name=(cronname+"pd"), task=('flybox.celery.pd' + str(cronnameid) +'()'), enabled=False)
+            job = cron.new(command='curl '+ cronnamer.ipaddress +"/5/on", comment=(cronnamer.title+"pd"))
+            job.minute.every(str(int(pdtimelist[1])))
+            job.hour.every(str(int(pdtimelist[0])))
+            job.dow.on(*pddays)
 
-            f = open('/home/arif/flybox/flybox/flybox/celery.py','a')
-            f.write('\n'+ '@app.task')
+            job.enable(False)
+            cron.write()
 
-
-            f.write('\n'+ 'def pd'+ str(cronnameid) +'():')
-            f.write('\n'+ '    print("it working")')
-            f.write('\n'+ '    contents = urllib.request.urlopen("http://'+cronnamer.ipaddress +'/5/on").read()')
-             
-
-            f.close()
 
             return HttpResponse('Successfull')
 
         else:
             schedule2, _ = CrontabSchedule.objects.get_or_create(minute=pdtimelist[1],hour=pdtimelist[0],day_of_week=my_string,day_of_month='*', month_of_year='*',)
-            obj2.crontab=schedule2 
+            obj2.crontab=schedule2
             obj2.save
-            return HttpResponse("Successfully updated")            
+            return HttpResponse("Successfully updated")
 
 
 
@@ -178,35 +175,56 @@ def updatecontrolpd(request):
 
         return HttpResponseRedirect('')
 
-def startautomation(request, categ_name=None, subcateg_name=None):
-    print(categ_name)
+def startautomation(request):
 
-    buttonpressed = request.GET.get('buttonpressed')
-    pk = request.GET.get('name')
+    id = request.GET.get('dept_id')
+    buttonpressed = request.GET.get('user_name')
 
+    cronnamer= Flyboxset.objects.get(pk=id)
+
+    pk= cronnamer.title
     if buttonpressed=="startcd" :
+        Automation.objects.filter(flybox=Flyboxset.objects.get(pk=id)).update(cdenabled=True)
+
         obj2 = PeriodicTask.objects.get(name=(pk+"cd"))
-        obj2.enabled=True 
+        findjob= cron.find_comment(cronnamer.title+"cd")
+        for item1 in findjob:
+            print(item1)
+            item1.enable(True)
+
         print("startcd is s")
 
     elif buttonpressed=="stopcd":
+        Automation.objects.filter(flybox=Flyboxset.objects.get(pk=id)).update(cdenabled=False)
+
         obj2 = PeriodicTask.objects.get(name=(pk+"cd"))
-        obj2.enabled=False 
-        obj2.save
+        findjob= cron.find_comment(cronnamer.title+"cd")
+        for item1 in findjob:
+            print(item1)
+            item1.enable(False)
+        print("stopcd")
 
     elif buttonpressed=="startpd":
+        Automationpd.objects.filter(flybox=Flyboxset.objects.get(pk=id)).update(pdenabled=True)
+
         obj2 = PeriodicTask.objects.get(name=(pk+"pd"))
-        obj2.enabled=True
-        obj2.save
+        findjob= cron.find_comment(cronnamer.title+"pd")
+        for item1 in findjob:
+            print(item1)
+            item1.enable(True)
+
+        print("startcd is s")
 
     elif buttonpressed=="stoppd":
+        Automationpd.objects.filter(flybox=Flyboxset.objects.get(pk=id)).update(pdenabled=False)
+
         obj2 = PeriodicTask.objects.get(name=(pk+"pd"))
-        obj2.enabled=False  
-        obj2.save
+        findjob= cron.find_comment(cronnamer.title+"pd")
+        for item1 in findjob:
+            print(item1)
+            item1.enable(False)
 
 
- 
+
     print(buttonpressed)
-    return HttpResponse("Successfully updated")            
-
-
+    return HttpResponseRedirect('/')
